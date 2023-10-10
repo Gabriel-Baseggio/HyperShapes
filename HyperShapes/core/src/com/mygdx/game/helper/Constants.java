cd815639cdccaf9c1238468be25111e918b8ad40
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2023-10-10 14:57:46 -0300

    .

--- HyperShapes/core/src/com/mygdx/game/helper/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.mygdx.game.helper;
-
-public class Constants {
-
-    public static final float PPM = 32;
-
-}
--- HyperShapes/core/src/com/mygdx/game/helper/ContactType.java
+++
@@ -1,7 +0,0 @@
-package com.mygdx.game.helper;
-
-public enum ContactType {
-
-    WALL, PLAYER, PLAYERPROJECTILE, BOSS, BOSSPROJECTILE, SENSOR
-
-}
