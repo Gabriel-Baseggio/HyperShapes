32290fbbb3b172a72e50d8c192ccf01c572846e2
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2023-10-31 15:11:54 -0300

    a2

--- core/src/com/mygdx/game/entities/Player.java
+++ core/src/com/mygdx/game/entities/Player.java
@@ -94,7 +94,7 @@ public class Player {
         if (HyperShapes.INSTANCE.getHighscore() < (int) defineDifficulty(this.score)) {
             HyperShapes.INSTANCE.setHighscore((int) defineDifficulty(this.score));
         }
-        this.setScore(HyperShapes.INSTANCE.getHighscore());
+        this.setScore((int) defineDifficulty(this.score));
         this.lost = true;
     }
 
