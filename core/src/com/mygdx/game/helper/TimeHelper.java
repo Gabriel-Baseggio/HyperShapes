29e956fb93047730e1f2bdab2c8082967c27350a
JoaoVitorWinter <joaovwinter3108@gmail.com>
2023-11-07 14:32:48 -0300

    JavaDoc da classe TimeHelper | 07/11 14:32

--- core/src/com/mygdx/game/helper/TimeHelper.java
+++ core/src/com/mygdx/game/helper/TimeHelper.java
@@ -2,13 +2,27 @@ package com.mygdx.game.helper;
 
 import com.badlogic.gdx.Gdx;
 
+/**
+ * Classe de auxílio para realizar o manuseio no tempo do mundo criado para o jogo.
+ */
 public class TimeHelper {
+    /**
+     * Propriedade do tipo float que representa a escala de tempo no mundo, sendo utilizada para multiplicar. Um número menor que 1 representa um slow motion.
+     */
     private float timeScale = 1.0f;
 
+    /**
+     * Método que é utilizado para alcançar o encapsulamento da propriedade timeScale, mudando o valor dela conforme o parâmetro
+     * @param scale (float)
+     */
     public void setTimeScale(float scale) {
         this.timeScale = scale;
     }
 
+    /**
+     * Método que é utilizado para retornar o deltaTime vezes o timeScale, permitindo que seja possível criar uma sensação de slow motion.
+     * @return deltaTime (float)
+     */
     public float getDeltaTime() {
         return Gdx.graphics.getDeltaTime() * timeScale;
     }
