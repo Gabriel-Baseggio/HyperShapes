1de857f9db7493d9e68a6f9accff8709353d4a76
JoaoVitorWinter <joaovwinter3108@gmail.com>
2023-11-07 16:39:46 -0300

    JavaDoc da classe DesktopLauncher | 07/11 16:39

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -3,7 +3,14 @@ package com.mygdx.game;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
+/**
+ * Classe que iniciará o jogo.
+ */
 public class DesktopLauncher {
+	/**
+	 * Método que será chamado ao rodar o projeto.
+	 * @param arg (String[])
+	 */
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
