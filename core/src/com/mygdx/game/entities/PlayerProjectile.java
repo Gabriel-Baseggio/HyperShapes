469bb51fd7d36139b65b3a93e64b28223e42866a
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2023-11-07 16:35:56 -0300

    Documentação do PlayerProjectile

--- core/src/com/mygdx/game/entities/PlayerProjectile.java
+++ core/src/com/mygdx/game/entities/PlayerProjectile.java
@@ -17,26 +17,69 @@ import static com.mygdx.game.helper.BodyHelper.createCircle;
 import static com.mygdx.game.helper.Constants.PPM;
 import static com.mygdx.game.helper.ContactType.PLAYERPROJECTILE;
 
+/**
+ * Classe que representa o projétil disparado pelo jogador no jogo.
+ */
 public class PlayerProjectile extends InputAdapter {
 
+    /**
+     * O corpo associado do projétil.
+     */
     private Body body;
+    /**
+     * A posição do projétil como um vetor 2D, com x e y.
+     */
     private Vector2 position;
+    /**
+     * A velocidade do projétil.
+     */
     private float speed;
+
+    /**
+     * O diâmetro do projétil.
+     */
     private int diameter;
-    private Texture texture;
+
+    /**
+     * A tela do jogo à qual o projétil pertence.
+     */
     private GameScreen gameScreen;
+
+    /**
+     * Uma sinalização indicando se o jogador pode disparar o projétil.
+     */
     private boolean canShoot;
+
+    /**
+     * Uma sinalização indicando se a linha de direção do tiro deve ser desenhada.
+     */
     private boolean drawLine;
+
+    /**
+     * Uma sinalização indicando se o jogador tocou na tela (touch down).
+     */
     private boolean touchedDown;
+
+    /**
+     * O atraso entre os disparos do projétil.
+     */
     private float shootDelay;
+
+    /**
+     * O tempo decorrido desde a última atualização.
+     */
     private float deltaTime;
 
+    /**
+     * Inicializa um novo objeto PlayerProjectile com base na tela de jogo fornecida.
+     *
+     * @param gameScreen A tela do jogo à qual o projétil pertence.
+     */
     public PlayerProjectile(GameScreen gameScreen) {
         this.position = randomSpawn();
         this.gameScreen = gameScreen;
         this.speed = 1000000;
         this.diameter = 28;
-        this.texture = new Texture("SaboneteRosa.png");
         this.body = createCircle(position.x, position.y, diameter, false, 1, gameScreen.getWorld(), PLAYERPROJECTILE, new FixtureDef());
 
         this.canShoot = false;
@@ -46,9 +89,13 @@ public class PlayerProjectile extends InputAdapter {
         this.touchedDown = false;
 
         Gdx.input.setInputProcessor(this);
-
     }
 
+    /**
+     * Atualiza a posição, o atraso entre disparos e outras propriedades do projétil.
+     *
+     * @param deltaTime O tempo decorrido desde a última atualização.
+     */
     public void update(float deltaTime) {
         this.deltaTime = deltaTime;
         position.x = body.getPosition().x * PPM - diameter;
@@ -63,27 +110,38 @@ public class PlayerProjectile extends InputAdapter {
 
     }
 
-    public void renderLine(ShapeRenderer s) {
+    /**
+     * Renderiza a linha de direção do tiro (se ativada) usando um ShapeRenderer.
+     *
+     * @param shape O ShapeRenderer usado para renderizar a linha de direção.
+     */
+    public void renderLine(ShapeRenderer shape) {
         if (drawLine) {
-//            Vector2 p1 = new Vector2(Gdx.input.getX(), (Gdx.graphics.getHeight() - Gdx.input.getY()));
-//            Vector2 p2 = new Vector2(x + diameter, y + diameter);
-//            Vector2 p3 = new Vector2(p2).sub(p1).scl(500f).add(p2);
-
             Vector2 p1 = new Vector2(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
             Vector2 p2 = new Vector2(position.x + diameter, position.y + diameter);
             Vector2 mouseToP2 = p2.cpy().sub(p1);
             Vector2 extendedP1 = p1.cpy().sub(mouseToP2.nor().scl(10000f));
 
-            s.setColor(Color.WHITE);
-            s.rectLine(extendedP1, p2, 2);
-//            s.rectLine(p2, p3, 2);
+            shape.setColor(Color.WHITE);
+            shape.rectLine(extendedP1, p2, 2);
         }
     }
 
+    /**
+     * Renderiza o projétil na tela usando um SpriteBatch e a textura fornecida.
+     *
+     * @param batch O SpriteBatch usado para renderização.
+     * @param textura A textura usada para representar o projétil.
+     */
     public void render(SpriteBatch batch, Texture textura) {
         batch.draw(textura, position.x + (diameter/2), position.y + (diameter/2), diameter, diameter);
     }
 
+    /**
+     * Gera uma posição de spawn aleatória para o projétil.
+     *
+     * @return Um vetor 2D representando a posição de spawn aleatória.
+     */
     public Vector2 randomSpawn() {
         int x, y;
 
@@ -102,26 +160,34 @@ public class PlayerProjectile extends InputAdapter {
         return new Vector2(x, y);
     }
 
-    public Body getBody() {
-        return body;
-    }
-
+    /**
+     * Define se o jogador pode disparar o projétil.
+     *
+     * @param canShoot Verdadeiro se o jogador pode disparar; falso caso contrário.
+     */
     public void setCanShoot(boolean canShoot) {
         this.canShoot = canShoot;
     }
 
+    /**
+     * Verifica se o jogador pode disparar o projétil.
+     *
+     * @return Verdadeiro se o jogador pode disparar; falso caso contrário.
+     */
     public boolean getCanShoot() {
         return canShoot;
     }
 
+    /**
+     * Aplica uma força para direcionar o projétil na direção do mouse.
+     *
+     * @param mouseX A coordenada X da posição do mouse.
+     * @param mouseY A coordenada Y da posição do mouse.
+     * @param deltaTime O tempo decorrido desde a última atualização.
+     */
     public void applyForceToFollowMouse(float mouseX, float mouseY, float deltaTime) {
         if (canShoot && shootDelay >= 3) {
             float angle = MathUtils.atan2(mouseY - body.getPosition().y, mouseX - body.getPosition().x);
-
-//            Vector2 p1 = new Vector2(mouseX, mouseY);
-//            Vector2 p2 = new Vector2(body.getPosition().x, body.getPosition().y);
-//            Vector2 p3 = new Vector2(p2).sub(p1).scl(500f).add(p2);
-//            float angle = MathUtils.atan2(p3.y - body.getPosition().y, p3.x - body.getPosition().x);
 
             float forceX = MathUtils.cos(angle) * speed * deltaTime;
             float forceY = MathUtils.sin(angle) * speed * deltaTime;
@@ -134,6 +200,15 @@ public class PlayerProjectile extends InputAdapter {
 
     }
 
+    /**
+     * Manipula o evento de toque (touch down) na tela.
+     *
+     * @param screenX A coordenada X do toque na tela.
+     * @param screenY A coordenada Y do toque na tela.
+     * @param pointer O ponteiro associado ao evento de toque.
+     * @param button  O botão do mouse associado ao evento de toque.
+     * @return Verdadeiro se o evento de toque foi manipulado com sucesso; falso caso contrário.
+     */
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         if (canShoot && shootDelay >= 3) {
@@ -145,6 +220,15 @@ public class PlayerProjectile extends InputAdapter {
         return true;
     }
 
+    /**
+     * Manipula o evento de liberação do toque na tela.
+     *
+     * @param screenX A coordenada X do toque liberado na tela.
+     * @param screenY A coordenada Y do toque liberado na tela.
+     * @param pointer O ponteiro associado ao evento de liberação de toque.
+     * @param button O botão do mouse associado ao evento de liberação de toque.
+     * @return Verdadeiro se o evento de liberação de toque foi manipulado com sucesso; falso caso contrário.
+     */
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
         if (canShoot && shootDelay >= 3 && touchedDown) {
@@ -158,6 +242,14 @@ public class PlayerProjectile extends InputAdapter {
         return true;
     }
 
+    /**
+     * Manipula o evento de arrastar (drag) na tela.
+     *
+     * @param screenX A coordenada X do arrasto na tela.
+     * @param screenY A coordenada Y do arrasto na tela.
+     * @param pointer O ponteiro associado ao evento de arrastar.
+     * @return Verdadeiro se o evento de arrastar na tela foi manipulado com sucesso; falso caso contrário.
+     */
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
         if (canShoot && shootDelay >= 3) {
@@ -169,6 +261,11 @@ public class PlayerProjectile extends InputAdapter {
         return true;
     }
 
+    /**
+     * Obtém o atraso entre os disparos do projétil.
+     *
+     * @return O atraso entre os disparos do projétil.
+     */
     public float getDelayShoot() {
         return shootDelay;
     }
