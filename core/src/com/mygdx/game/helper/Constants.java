ce70bb4d68ec8b85b2f195093ffd9431b706934b
JoaoVitorWinter <joaovwinter3108@gmail.com>
2023-11-07 15:18:17 -0300

    JavaDoc da classe Constants | 07/11 15:18

--- core/src/com/mygdx/game/helper/Constants.java
+++ core/src/com/mygdx/game/helper/Constants.java
@@ -1,7 +1,14 @@
 package com.mygdx.game.helper;
 
+/**
+ * Classe que armazena valores fixos (constantes) para srem utilizados no resto do código.
+ */
 public class Constants {
 
+    /**
+     * Propriedade do tipo float que representa o número de conversão de pixels para metros.
+     * Já que é uma costante, cria-se como static e final.
+     */
     public static final float PPM = 32;
 
 }
