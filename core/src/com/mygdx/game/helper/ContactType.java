46afb1066db110311d9fe3b473d99ded88c261b9
JoaoVitorWinter <joaovwinter3108@gmail.com>
2023-11-07 15:11:42 -0300

    JavaDoc da enum ContactType | 07/11 15:11

--- core/src/com/mygdx/game/helper/ContactType.java
+++ core/src/com/mygdx/game/helper/ContactType.java
@@ -1,5 +1,8 @@
 package com.mygdx.game.helper;
 
+/**
+ * Um enum utilizado para poder diferenciar que tipo de contato cada corpo carregado na tela representa.
+ */
 public enum ContactType {
 
     WALL, PLAYER, PLAYERPROJECTILE, BOSS, BOSSPROJECTILE, SENSOR, BOSSBAR
