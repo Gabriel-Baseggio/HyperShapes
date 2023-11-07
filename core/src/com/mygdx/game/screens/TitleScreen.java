990cf50a813a200402fa5a9291362f1f09ef9be8
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2023-11-07 14:01:18 -0300

    Revert "Updates & trying to bundle the game"
    This reverts commit 25b6584dc2976c732d98bf38cb8ac704c6f0dd39.

--- core/src/com/mygdx/game/screens/TitleScreen.java
+++ core/src/com/mygdx/game/screens/TitleScreen.java
@@ -3,32 +3,49 @@ package com.mygdx.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.mygdx.game.HyperShapes;
-import com.mygdx.game.helper.FontHelper;
 
 public class TitleScreen extends ScreenAdapter {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 
+	private FreeTypeFontGenerator generator;
+    private FreeTypeFontGenerator.FreeTypeFontParameter parameter;
+    private BitmapFont bitmap;
+	
 	public TitleScreen(OrthographicCamera camera) {
 		this.camera = camera;
 		this.batch = new SpriteBatch();
+
+		this.generator = new FreeTypeFontGenerator(Gdx.files.internal("font.ttf"));
+        this.parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        this.parameter.size = 30;
+        this.parameter.borderWidth = 1;
+        this.parameter.borderColor = Color.BLACK;
+        this.parameter.color = Color.WHITE;
+        this.bitmap = generator.generateFont(parameter);
+		
 	}
 	
 	public void update() {
 		if (Gdx.input.isKeyPressed(Input.Keys.NUM_1)) {
-			startGame(1);
+			HyperShapes.INSTANCE.setScreen(new GameScreen(this.camera));
+			HyperShapes.INSTANCE.setDifficulty(1);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.NUM_2)) {
-			startGame(2);
+			HyperShapes.INSTANCE.setScreen(new GameScreen(this.camera));
+			HyperShapes.INSTANCE.setDifficulty(2);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.NUM_3)) {
-			startGame(4);
+			HyperShapes.INSTANCE.setScreen(new GameScreen(this.camera));
+			HyperShapes.INSTANCE.setDifficulty(4);
 		}
 
 		if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
@@ -44,30 +61,26 @@ public class TitleScreen extends ScreenAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
         batch.begin();
-
+        
         batch.draw(new Texture("patinho.png"), HyperShapes.INSTANCE.getScreenWidth() / 2, HyperShapes.INSTANCE.getScreenHeight() / 2, 64 , 64);
         batch.draw(new Texture("patinho.png"), HyperShapes.INSTANCE.getScreenWidth() / 2 - 96, HyperShapes.INSTANCE.getScreenHeight() / 2, 64 , 64);
         batch.draw(new Texture("patinho.png"), HyperShapes.INSTANCE.getScreenWidth() / 2 - 80 , HyperShapes.INSTANCE.getScreenHeight() / 2 - 200, 128 , 128);
+        
+        bitmap.draw(batch, "Hyper Shapes", HyperShapes.INSTANCE.getScreenWidth() / 2 - 120, HyperShapes.INSTANCE.getScreenHeight() / 2);
 
-		FontHelper.write(batch, "Pressione ESC para sair", new Vector2(10, HyperShapes.INSTANCE.getScreenHeight() - 30), 30);
+        bitmap.draw(batch, "Pressione ESC para sair", 10, HyperShapes.INSTANCE.getScreenHeight() - parameter.size);
 
-		FontHelper.write(batch, "Hyper Shapes", new Vector2(HyperShapes.INSTANCE.getScreenWidth() / 2 - 120, HyperShapes.INSTANCE.getScreenHeight() / 2), 50);
-
-		FontHelper.write(batch, "Pressione 1 para jogar na dificuldade muito fácil", new Vector2(10, 150), 30);
-		FontHelper.write(batch, "Pressione 2 para jogar na dificuldade levemente fácil", new Vector2(10, 90), 30);
-		FontHelper.write(batch, "Pressione 3 para jogar na dificuldade fácil", new Vector2(10, 30), 30);
+        bitmap.draw(batch, "Pressione 1 para jogar na dificuldade muito fácil", 10, parameter.size*5);
+        bitmap.draw(batch, "Pressione 2 para jogar na dificuldade levemente fácil", 10, parameter.size*3);
+        bitmap.draw(batch, "Pressione 3 para jogar na dificuldade fácil", 10, parameter.size);
 
 		batch.end();
 	}
 
 	@Override
 	public void dispose() {
+		bitmap.dispose();
+		generator.dispose();
 		batch.dispose();
 	}
-
-	public void startGame(int difficulty){
-		HyperShapes.INSTANCE.setDifficulty(difficulty);
-		HyperShapes.INSTANCE.setScreen(new GameScreen(this.camera));
-	}
-
 }
